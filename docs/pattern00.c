568c7207c719bd9431f8b9a8f7fb7c8befd1549c
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-03-06 18:36:01 +0530

    sample codes

---
+++ docs/pattern00.c
@@ -0,0 +1,14 @@
+#include "telugu.h"
+
+sankya aarambham()
+{
+    // write your commands
+    // to the computer here
+    numberkottu(42);
+    enterkottu();
+    enterkottu();
+
+
+
+    venakkivvu 0;
+}
